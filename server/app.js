53ed2a531294f9a667af14bbdda4ba49b6df4f13
Nikita <nikitagryaznov1997@gmail.com>
2025-09-08 20:57:38 +0700

    feat: Инициализировал серверную часть приложения

---
+++ server/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Сервер работает на порту ${process.env.PORT}`);
+});
